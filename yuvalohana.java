002e71d5b556ae4c2e0d53549650df6bd2dd45a2
yuval ohana <yuvalohana10@gmail.com>
2022-11-23 13:25:30 +0200

    changing yuvalohana file

--- yuvalohana.java
+++ yuvalohana.java
@@ -1,7 +1,7 @@
 public class yuvalohana {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++){
-			System.out.println("Yuval Ohana"+i);
+			System.out.println("I change my name!"+i);
 		}
 	}
 }
