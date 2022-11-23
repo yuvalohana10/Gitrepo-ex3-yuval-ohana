0f41116fde4858f18ac5d0358303a24522404d2b
yuval ohana <yuvalohana10@gmail.com>
2022-11-23 13:26:20 +0200

    fix typo

--- yuvalohana.java
+++ yuvalohana.java
@@ -1,7 +1,7 @@
 public class yuvalohana {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++){
-			System.out.println("I change my name!"+i);
+			System.out.println("I changed my name!"+i);
 		}
 	}
 }
